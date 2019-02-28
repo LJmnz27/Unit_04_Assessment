ea966f28a7ff191c41dc73e9049a54bcede7c20d
Luis Jimenez <luisjimenez@Luiss-MacBook-Pro.local>
2019-02-27 20:28:42 -0500

    Halfway mark

--- app/src/main/java/org/pursuit/unit_04_assessment/controller/EchinodermsAdapter.java
+++ app/src/main/java/org/pursuit/unit_04_assessment/controller/EchinodermsAdapter.java
@@ -2,8 +2,11 @@ package org.pursuit.unit_04_assessment.controller;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import org.pursuit.unit_04_assessment.R;
 import org.pursuit.unit_04_assessment.views.EchinodermsViewHolder;
 
 import java.util.List;
@@ -18,16 +21,17 @@ public class EchinodermsAdapter extends RecyclerView.Adapter<EchinodermsViewHold
     @NonNull
     @Override
     public EchinodermsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View childView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.echinoderms_item_view, viewGroup, false);
+        return new EchinodermsViewHolder(childView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull EchinodermsViewHolder echinodermsViewHolder, int i) {
-
+        echinodermsViewHolder.onBind(echinodermsList.get(i));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return echinodermsList.size();
     }
 }
--- app/src/main/java/org/pursuit/unit_04_assessment/views/EchinodermsViewHolder.java
+++ app/src/main/java/org/pursuit/unit_04_assessment/views/EchinodermsViewHolder.java
@@ -1,12 +1,38 @@
 package org.pursuit.unit_04_assessment.views;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.pursuit.unit_04_assessment.models.MessageItem;
 
 public class EchinodermsViewHolder extends RecyclerView.ViewHolder {
+    private static final String TAG = "image_call";
+    private SharedPreferences sharedPreferences;
+    private TextView echinodermsTextView;
+    private Intent intent;
+    private ImageView imageView;
+
 
     public EchinodermsViewHolder(@NonNull View itemView) {
         super(itemView);
+    }
+
+    public void onBind (String echinoderm){
+        MessageItem messageItem = new MessageItem();
+        echinodermsTextView.setText(echinoderm);
+        Picasso.get().load(messageItem.getImage()).into(imageView);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
